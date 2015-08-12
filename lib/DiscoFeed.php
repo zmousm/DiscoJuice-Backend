4d72489a771d8c03fc727c1ec2d12b136d866964
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2015-08-12 13:02:52 +0200

    Remove debugging logging. Fixes andreassolberg/DiscoJuice#51

--- lib/DiscoFeed.php
+++ lib/DiscoFeed.php
@@ -49,7 +49,6 @@ class DiscoFeed {
 			if (isset($metadataEntry["EntityAttributes"]) && isset($metadataEntry["EntityAttributes"]["http://macedir.org/entity-category"])) {
 				if (in_array("http://refeds.org/category/hide-from-discovery", $metadataEntry["EntityAttributes"]["http://macedir.org/entity-category"])) {
 					DiscoUtils::log('Skipping entry because refeds.org../hide-from-discovery ' . $entityId . "");
-					print_r($metadataEntry); exit;
 					continue;
 				}
 				
